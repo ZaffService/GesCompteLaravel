0a2e5652647003c69abfc245d407eb58160a96b2
ZaffService <seckmoustapha238@gmail.com>
2025-10-26 17:06:51 +0000

    Fix database URL parsing and Swagger UI assets serving

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -91,7 +91,7 @@ return [
             /*
              * Edit to set the api's base path
              */
-            'base' => env('L5_SWAGGER_BASE_PATH', env('APP_URL', 'https://moustapha-seck.onrender.com')),
+            'base' => env('L5_SWAGGER_BASE_PATH', null),
 
             /*
              * Absolute path to directories that should be excluded from scanning
