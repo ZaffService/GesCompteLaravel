393e598e612403e119c695ead5e68d6a1986c580
ZaffService <seckmoustapha238@gmail.com>
2025-10-27 01:00:36 +0000

    Add redirect from /docs to /api/documentation for proper Swagger UI access

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('/', function () {
 });
 
 Route::get('/docs/api-docs.json', [SwaggerDocsController::class, 'getJson'])->name('swagger.json');
+Route::get('/docs', function () {
+    return redirect('/api/documentation');
+});
