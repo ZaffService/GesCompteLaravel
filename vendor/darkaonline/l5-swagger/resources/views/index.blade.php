c67405fdae9427effa9deb864ca21b86718e5f20
ZaffService <seckmoustapha238@gmail.com>
2025-10-27 04:17:07 +0000

    Leave Swagger UI Explore field empty by default

--- vendor/darkaonline/l5-swagger/resources/views/index.blade.php
+++ vendor/darkaonline/l5-swagger/resources/views/index.blade.php
@@ -126,7 +126,7 @@
         // Build a system
         const ui = SwaggerUIBundle({
             dom_id: '#swagger-ui',
-            url: "https://moustapha-seck.onrender.com/docs/api-docs.json",
+            url: "",
             operationsSorter: {!! isset($operationsSorter) ? '"' . $operationsSorter . '"' : 'null' !!},
             configUrl: {!! isset($configUrl) ? '"' . $configUrl . '"' : 'null' !!},
             validatorUrl: {!! isset($validatorUrl) ? '"' . $validatorUrl . '"' : 'null' !!},
